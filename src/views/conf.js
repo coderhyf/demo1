8b0cc88eaf0fc4a68b5b0e39436aad818abeda49
coderhyf <haofh@coderhyf-3.local>
2023-11-09 09:02:27 +0800

    new branch

---
+++ src/views/conf.js
@@ -0,0 +1,17 @@
+ export function defer(maxFrameCount = 1000) {
+  let count = 0
+  // const frameCount = ref(0);
+  // console.log(frameCount, '===frameCount');
+  const refreshFrameCount = () => {
+    requestAnimationFrame(() => {
+      count++;//计数帧数加一
+      if (count < maxFrameCount) {
+        refreshFrameCount();//只要没到最大帧数就一直持续调用函数
+      }
+    });
+  };
+  refreshFrameCount();
+  return function (showInframeCoount) {
+    return count >= showInframeCoount;//判断当前渲染帧数又是否大于自定义n
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,40 @@ module.exports = {
   devServer: {
     port: 8081,     // 端口号
   },
-
+  chainWebpack(config){
+  
+        config.optimization.splitChunks({
+            cacheGroups:{
+                common: {//commons 一般是是个人定义的
+                    name: 'chunk-common', // 打包后的文件名
+                    chunks: 'initial',
+                    minChunks: 1,
+                    maxInitialRequests: 5,
+                    minSize: 0,
+                    priority: 1,
+                    reuseExistingChunk: true
+                },
+                vendors: {//vendor 是导入的 npm 包
+                    name: 'chunk-vendors',
+                    test: /[\\/]node_modules[\\/]/,
+                    chunks: 'initial',
+                    // maxSize: 600000,
+                    // maxInitialRequests: 20,
+                    priority: 2,
+                    // reuseExistingChunk: true,
+                    // enforce: true
+                },
+                element: {//把antDesignVue从chunk-vendors.js提取出来。当然我们也可以把mixins，vue.min.js等等也按照类似配置提取出来
+                    name: 'chunk-element',
+                    test: /[\\/]element-ui[\\/]/,
+                    chunks: 'initial',
+                    priority: 3,
+                    // maxSize: 600000,
+                    reuseExistingChunk: true,
+                    // enforce: true
+                }
+            }
+        })
+    
+}, 
 };
